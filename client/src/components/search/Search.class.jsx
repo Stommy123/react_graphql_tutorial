2548fbff344306c62fe320edb5fb3fdc8722379f
tommy <stommy49@yahoo.com>
2019-09-28 21:43:58 -0400

    memo debounce

---
+++ client/src/components/search/Search.class.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import debounce from 'debounce-promise';
+import { Icon } from '..';
+import ReactSelect from 'react-select';
+import AsyncSelect from 'react-select/async';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    const loadOptions = inputValue => props.loadOptions(inputValue);
+    this.debouncedLoadOptions = debounce(loadOptions, 1000, { leading: true });
+    console.log('debounce', this.debouncedLoadOptions);
+    this.state = { active: false };
+  }
+  handleFocus = _ => this.setState({ active: true });
+  handleBlur = _ => this.setState({ active: false });
+  handleInputChange = input => {
+    const { onInputChange } = this.props;
+    return onInputChange ? onInputChange(input) : input;
+  };
+  handleChange = value => {
+    const { id, onChange } = this.props;
+    onChange && onChange({ id, value });
+    this.selectRef.blur();
+  };
+  customStyles = _ => ({
+    dropdownIndicator: base => ({
+      ...base,
+      transition: 'all .2s ease'
+    })
+  });
+  customComponents = _ => ({
+    DropdownIndicator: _ => <Icon className="search" icon="search" />
+  });
+  emptyOptions = ({ inputValue }) => <div>{inputValue ? 'No Results Found' : 'Type To Search'}</div>;
+  render() {
+    const { active } = this.state;
+    const { async, isMulti, label, placeholder, options = [], className, value } = this.props;
+    const Select = async ? AsyncSelect : ReactSelect;
+    return (
+      <>
+        {label && <label>{label}</label>}
+        <Select
+          ref={ref => (this.selectRef = ref)}
+          placeholder={placeholder}
+          components={this.customComponents()}
+          customStyles={this.customStyles()}
+          classNamePrefix="select"
+          className={classNames('react-select', className, { active })}
+          noOptionsMessage={this.emptyOptions}
+          isSearchable
+          isMulti={isMulti}
+          loadOptions={this.debouncedLoadOptions}
+          options={options}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          onInputChange={this.handleInputChange}
+          onChange={this.handleChange}
+          value={value}
+          isClearable
+        />
+      </>
+    );
+  }
+}
+
+export default Search;
--- client/src/components/search/Search.component.jsx
+++ client/src/components/search/Search.component.jsx
@@ -1,67 +1,67 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import debounce from 'debounce-promise';
-import { Icon } from '..';
-import ReactSelect from 'react-select';
+import React, { useState, useCallback, useRef } from 'react';
 import AsyncSelect from 'react-select/async';
+import ReactSelect from 'react-select';
+import debounce from 'debounce-promise';
+import classNames from 'classnames';
+import { Icon } from '..';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    const loadOptions = inputValue => props.loadOptions(inputValue);
-    this.debouncedLoadOptions = debounce(loadOptions, 1000, { leading: true });
-    this.state = { active: false };
-  }
-  handleFocus = _ => this.setState({ active: true });
-  handleBlur = _ => this.setState({ active: false });
-  handleInputChange = input => {
-    const { onInputChange } = this.props;
-    return onInputChange ? onInputChange(input) : input;
-  };
-  handleChange = value => {
-    const { id, onChange } = this.props;
+const Search = ({
+  id,
+  async,
+  isMulti,
+  loadOptions,
+  onChange,
+  onInputChange,
+  label,
+  placeholder,
+  options = [],
+  className,
+  value
+}) => {
+  const selectRef = useRef(null);
+  const [active, setActive] = useState(false);
+  const handleFocus = _ => setActive(true);
+  const handleBlur = _ => setActive(false);
+  const handleInputChange = input => onInputChange && onInputChange(input);
+  const handleChange = value => {
     onChange && onChange({ id, value });
-    this.selectRef.blur();
+    selectRef.current.blur();
   };
-  customStyles = _ => ({
+  const customStyles = _ => ({
     dropdownIndicator: base => ({
       ...base,
       transition: 'all .2s ease'
     })
   });
-  customComponents = _ => ({
+  const customComponents = _ => ({
     DropdownIndicator: _ => <Icon className="search" icon="search" />
   });
-  emptyOptions = ({ inputValue }) => <div>{inputValue ? 'No Results Found' : 'Type To Search'}</div>;
-  render() {
-    const { active } = this.state;
-    const { async, isMulti, label, placeholder, options = [], className, value } = this.props;
-    const Select = async ? AsyncSelect : ReactSelect;
-    return (
-      <>
-        {label && <label>{label}</label>}
-        <Select
-          ref={ref => (this.selectRef = ref)}
-          placeholder={placeholder}
-          components={this.customComponents()}
-          customStyles={this.customStyles()}
-          classNamePrefix="select"
-          className={classNames('react-select', className, { active })}
-          noOptionsMessage={this.emptyOptions}
-          isSearchable
-          isMulti={isMulti}
-          loadOptions={this.debouncedLoadOptions}
-          options={options}
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-          onInputChange={this.handleInputChange}
-          onChange={this.handleChange}
-          value={value}
-          isClearable
-        />
-      </>
-    );
-  }
-}
+  const emptyOptions = ({ inputValue }) => <div>{inputValue ? 'No Results Found' : 'Type To Search'}</div>;
+  const Select = async ? AsyncSelect : ReactSelect;
+  const memoizedLoadOptions = useCallback(debounce(loadOptions, 1000, { leading: true }), []);
+  return (
+    <>
+      {label && <label>{label}</label>}
+      <Select
+        ref={selectRef}
+        placeholder={placeholder}
+        components={customComponents()}
+        customStyles={customStyles()}
+        classNamePrefix="select"
+        className={classNames('react-select', className, { active })}
+        noOptionsMessage={emptyOptions}
+        isSearchable
+        isMulti={isMulti}
+        loadOptions={memoizedLoadOptions}
+        options={options}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onInputChange={handleInputChange}
+        onChange={handleChange}
+        value={value}
+      />
+    </>
+  );
+};
 
 export default Search;
